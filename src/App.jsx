aa9ea745e943ed52e1fded5061ef84051a7efcca
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-09-15 19:04:33 +0200

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Navbar } from './components'
+import { Feed, ChannelDetail, SearchFeed, VideoDetail } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" exact element={<VideoDetail />} />
+                <Route path="/channel/:id" exact element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
 }
 
